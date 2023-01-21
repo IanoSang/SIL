a5a7cf64448192f253862fb21ad1f1489e94dba0
IanSang <ianosang18@gmail.com>
2023-01-21 14:55:16 +0300

    feat: views- adds a post funtions

--- app/views.py
+++ app/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from django.http import HttpResponse
+from .serializers import ImageSerializer
+from .models import Image
+
 
 # Create your views here.
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Image.objects.all()
+    serializer_class = ImageSerializer
+
+    def post(self, request, *args, **kwargs):
+        photo = request.data['photo']
+        photo_name = request.data['photo_name']
+        Image.objects.create(photo_name=photo_name, photo=photo)
+        return HttpResponse({'message': 'Photo uploaded'}, status=200)
